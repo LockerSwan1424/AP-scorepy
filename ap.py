379458501da80b9b46fdf5294bb380682910c87c
LockerSwan1424 <LockerSwan12@protonmail.com>
2025-12-23 09:38:40 -0800

    Add files via upload

---
+++ ap.py
@@ -0,0 +1,9 @@
+def main():
+    Score = int(input("What is year AP score"))
+    if Score <= 2:
+        print("You not pass your AP test score")
+    else:
+        print("You pass your AP test score")
+    
+    
+main()
